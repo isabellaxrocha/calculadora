4f5fa68324c049393b85906b8d53f6ccce6c6e8c
isabellaxrocha <68255650+isabellaxrocha@users.noreply.github.com>
2020-07-21 15:04:05 -0300

    Foi feito o upload de todo o projeto.
    Este projeto é a versão 0.1 da calculadora.

---
+++ CALCULADORA_1CD_V_0_1_1/CALCULADORA_1CD_V_0_1/fm_Ajuda.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CALCULADORA_1CD_V_0_1
+{
+    partial class fm_Ajuda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fm_Ajuda));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(377, 216);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // fm_Ajuda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 241);
+            this.Controls.Add(this.textBox1);
+            this.Name = "fm_Ajuda";
+            this.Text = "Ajuda da Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ CALCULADORA_1CD_V_0_1_1/CALCULADORA_1CD_V_0_1/fm_Ajuda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CALCULADORA_1CD_V_0_1
+{
+    public partial class fm_Ajuda : Form
+    {
+        public fm_Ajuda()
+        {
+            InitializeComponent();
+        }
+    }
+}
